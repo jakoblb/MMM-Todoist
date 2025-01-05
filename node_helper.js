9c320a4a3635722cb22159bf5f4932aa7e443607
Jakob Lindbjerg Buthler <jakoblb@gmail.com>
2025-01-06 00:29:25 +0100

    Complete by touch

--- node_helper.js
+++ node_helper.js
@@ -30,6 +30,11 @@ module.exports = NodeHelper.create({
 			this.config = payload;
 			this.testCompleted();
 		}
+		else if(notification === "COMPLETE_ITEM")
+		{
+			this.config = payload.payload;
+			this.completeItem(payload.id);
+		}
 	},
 
 	fetchTodos : function() {
@@ -96,6 +101,61 @@ module.exports = NodeHelper.create({
 				since: dateString,
 				annotate_items: true,
 			}
+		},
+		function(error, response, body) {
+			if (error) {
+				self.sendSocketNotification("FETCH_ERROR", {
+					error: error
+				});
+				return console.error(" ERROR - MMM-Todoist: " + error);
+			}
+			if(self.config.debug){
+				console.log(body);
+			}
+			if (response.statusCode === 200) {
+				var taskJson = JSON.parse(body);
+				taskJson.items.forEach((item)=>{
+					if(item.item_object!=undefined)
+					{
+						item.item_object.contentHtml = markdown.makeHtml(item.content);
+					}
+				});
+				taskJson.accessToken = acessCode;
+				self.sendSocketNotification("TASKS_INC_COMPLETED", taskJson);
+			}
+			else{
+				console.log("Todoist api request status="+response.statusCode);
+			}
+		});
+	},
+
+	completeItem : function(id) {
+		var self = this;
+		//request.debug = true;
+		var acessCode = self.config.accessToken;
+		var dateToLookbackForCompleted = new Date(Date.now() - self.config.maksCompletedAgeDays * 24 * 60 * 60 * 1000);
+		var dateString = dateToLookbackForCompleted.getFullYear()+"-"+String(dateToLookbackForCompleted.getMonth()+1).padStart(2, '0')+"-"+String(dateToLookbackForCompleted.getDate()).padStart(2, '0')+"T00:00:00.000000Z";
+		request({
+			url: self.config.apiBase + "/" + self.config.apiVersion + "/" + self.config.todoistEndpointCompleted + "/",
+			method: "POST",
+			headers: {
+				"content-type": "application/x-www-form-urlencoded",
+				"cache-control": "no-cache",
+				"Authorization": "Bearer " + acessCode
+			},
+			form: {
+				commands:
+				[
+					{
+						type: "item_complete",
+						uuid: "",
+						args:
+						{
+							id: id,
+						}
+					}
+				]
+			},
 		},
 		function(error, response, body) {
 			if (error) {
